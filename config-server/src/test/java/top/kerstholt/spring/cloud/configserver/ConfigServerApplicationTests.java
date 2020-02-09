16f63215331d631015914e67e997622d47cede6c
Roelfie <roelfie.kerstholt@gmail.com>
2020-02-09 22:18:03 +0100

    Empty Spring Boot project 'config-server' with dependency on spring-cloud-config-server.

---
+++ config-server/src/test/java/top/kerstholt/spring/cloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kerstholt.spring.cloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
